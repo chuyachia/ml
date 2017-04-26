33ce90ea78a402c9f91838520ca31fd1d579d117
chuyachia <chuyachia@gmail.com>
2017-04-26 17:17:55 -0400

    defined functions

--- boosting/adaboost.R
+++ boosting/adaboost.R
@@ -2,73 +2,80 @@
 ## binary classification that uses decision stumps as the base algorithm.
 ## The iris dataset is used as an example here.
 
-## load data
-iris_bi <- iris
-iris_bi$Species <- ifelse(iris$Species=="virginica",1,-1)
+#### Load data
+data(iris)
+iris$Species <- ifelse(iris$Species=="versicolor",1,-1)
 
-## seperate train and test
+#### Seperate train and test
 train_indx<-sample(150,100)
-X <- iris_bi[train_indx,c(1:4)]
-Y <- iris_bi[train_indx,5]
-n <- nrow(X)
-f <- ncol(X)
+X_train <- iris[train_indx,c(1:4)]
+Y_train <- iris[train_indx,5]
+X_test <- iris[-train_indx,c(1:4)]
+Y_test <- iris[-train_indx,5]
 
-## parameters
-# number of models to generate
-nMod <- 20
-
-## initialize to uniform vote
-w <- rep(1/n,n)
-vote_df <- NULL
-predict_df <- NULL
-dir_df <- NULL
-thres_df <- NULL
-feature_df <- NULL
-for (j in 1 : nMod)
-{
+#### Define functions
+train <- function(X,Y,W){
+  f <- ncol(X)
+  n <- nrow(X)
   thres <- rep(NA,f)
   dir<-  rep(NA,f)
   e <-  rep(NA,f)
-  predict <- matrix(nrow=n,ncol=f)
-  ## train one decision stump on each feature
+  fit <- matrix(nrow=n,ncol=f)
   for (i in 1 :f)
   {
     indx <- order(X[,i])
-    x_order <- X[indx,i]
-    y_order <- Y[indx]* w[indx]
+    x_order <- X[,i]
+    y_order <- Y* W
     data_order <- aggregate(y_order~x_order,FUN=sum)
     max_ind <-which.max(abs(cumsum(data_order$y_order)))
     thres[i] <- data_order$x_order[max_ind]
     dir[i] <- -sign(cumsum(data_order$y_order)[max_ind])
-    predict[,i] <-  dir[i]*ifelse((X[,i]-thres[i])>0,1,-1)
-    e[i] <-  sum((Y!=predict[,i])*w)/sum(w)
+    fit[,i] <-  dir[i]*ifelse((X[,i]-thres[i])>0,1,-1)
+    e[i] <-  sum((Y!=fit[,i])*W)/sum(W)
   }
-  ## choose the best feature to use
   feature <- which.min(e)
-  thres_df <- rbind(thres_df,thres[feature])
-  dir_df <- rbind(dir_df,dir[feature])
-  feature_df<- rbind(feature_df,feature)
-  ## predict
-  P <- predict[,feature]
-  E <- e[feature]
-  S <- sqrt((1-E)/E)
-  ## update weights (so that the next model will give different results)
-  w[which(Y!=P)] <- w[which(Y!=P)]*S
-  w[which(Y==P)] <- w[which(Y==P)]/S
-  vote <- log(S)
-  predict_df<- cbind(predict_df,P)
-  vote_df <- rbind(vote_df,vote)
+  model <- list(feature=feature, 
+                threshold=thres[feature],
+                direction= dir[feature],
+                fit = fit[,feature],
+                error= e[feature])
+  return(model)
 }
 
-## In-sample error
-length(which(sign(predict_df %*% vote_df)!=Y))/length(Y)
+predict <- function(model,X){
+  model$direction*ifelse((X[,mod$feature]-mod$threshold)>0,1,-1)
+}
 
-## out-of-sampe error
-X_test <- iris_bi[-train_indx,c(1:4)]
-Y_test <- iris_bi[-train_indx,5]
-X_expand <- sapply(feature_df,function(x)X_test[,x])
-X_expand<-sapply(1:ncol(X_expand),function(x){
-  dir_df[x]*ifelse((X_expand[,x]-thres_df[x])>0,1,-1)})
+#### Adaboost
+fit_df <- NULL
+predict_df <- NULL
+vote_df <- NULL
+w <- rep(1/length(Y_train),length(Y_train))
+nMod <- 20
+j <- 0
 
-length(which(sign(X_expand%*%vote_df)!=Y_test))/length(Y_test)
+while(j <nMod) {
+## train
+mod <- train(X=X_train,Y=Y_train,W=w)
+
+scale <-sqrt((1-mod$error)/mod$error)
+vote <- log(scale)
+vote_df <- rbind(vote_df,vote)
+fit_df <- rbind(fit_df,mod$fit)
+## predict
+pred <- predict(model=mod,X= X_test)
+predict_df <- rbind(predict_df,pred)
+## update
+w[which(Y_train!=mod$fit)] <- w[which(Y_train!=mod$fit)]*scale
+w[which(Y_train==mod$fit)] <- w[which(Y_train==mod$fit)]/scale
+j <- j+1
+if(mod$error==0) break
+}
+## aggregate
+agg_fit <- t(fit_df)%*%vote_df
+agg_predict <- t(predict_df)%*%vote_df
+## test error
+in_err <- length(which(sign(agg_fit)!=Y_train))/length(Y_train)
+out_err <- length(which(sign(agg_predict)!=Y_test))/length(Y_test) 
+
 
